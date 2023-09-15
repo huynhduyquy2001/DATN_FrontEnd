720369cd0d8993261ce989b172ed1c10ed81052c
Vyx-stack <133791259+Vyx-stack@users.noreply.github.com>
2023-09-15 16:09:38 +0700

    4:09 | 15 - 09 - 2023

--- js/main.js
+++ js/main.js
@@ -42,7 +42,14 @@ app.config(function ($translateProvider, $routeProvider) {
 	}).when('/mystore', {
 		templateUrl: "ngview/myStore.html",
 		controller: 'MyStoreController'
-	});
+	}).when('/staff/postviolationmanagement', {
+		templateUrl: "ngview/PostsViolationsManagement.html",
+		controller: 'PvmCtrl'
+	}).when('/staff/violationtypemanagement', {
+		templateUrl: "ngview/ViolationTypeManagement.html",
+		controller: 'VtmCtrl'
+	})
+	;
 	// Set the default language
 	var storedLanguage = localStorage.getItem('myAppLangKey') || 'vie';
 	$translateProvider.preferredLanguage(storedLanguage);
