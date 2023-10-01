3c1916dc7cf08842a209c6924406b9490b1d5046
Vyx-stack <133791259+Vyx-stack@users.noreply.github.com>
2023-10-01 19:39:30 +0700

    1-10-2023 | 7:39

--- js/postsProduct.js
+++ js/postsProduct.js
@@ -1,98 +1,174 @@
 app.controller('productPostCtrl', function ($scope, $http, $translate, $rootScope, $location) {
-    $scope.listPosts = [];
-    $scope.currentPage = 0;
-    $scope.totalPages = 0;
-    $scope.pageSize = 9;
-    $scope.selectedCountText = 0; // Số lượng mục đã chọn
+	$scope.listPosts = [];
+	$scope.Product = {};
+	$scope.currentPage = 0;
+	$scope.totalPages = 0;
+	$scope.pageSize = 9;
+	$scope.selectedCountText = 0; // Số lượng mục đã chọn
 
-    var url = "http://localhost:8080";
+	var url = "http://localhost:8080";
 
-    $http.get(url + '/staff/postsproduct').then(function (response) {
-        // Gán dữ liệu từ API vào biến $scope.listViolations
-        $scope.listPosts = response.data;
-        console.log($scope.listPosts.content);
-    }).catch(function (error) {
-        console.error('Lỗi khi lấy dữ liệu bài đăng sản phẩm:', error);
-    });
+	
+
+	$scope.getproductList = function (currentPage) {
+
+		$http.get(url + "/staff/postsproduct/" + currentPage)
+			.then(function (res) {
+				$scope.listPosts = res.data.content; // Lưu danh sách sản phẩm từ phản hồi
+				$scope.totalPages = res.data.totalPages; // Lấy tổng số trang từ phản hồi				
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	$scope.getproductDecline = function (currentPage) {
+
+		$http.get(url + "/staff/postsProductDecline/" + currentPage)
+			.then(function (res) {
+				$scope.listPosts = res.data.content; // Lưu danh sách sản phẩm từ phản hồi
+				$scope.totalPages = res.data.totalPages; // Lấy tổng số trang từ phản hồi				
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	$scope.getproductList($scope.currentPage);
+
+	// $http.get(url + '/staff/postsproduct').then(function (response) {
+	// 	// Gán dữ liệu từ API vào biến $scope.listViolations
+	// 	$scope.listPosts = response.data;
+	// 	console.log($scope.listPosts.content);
+	// }).catch(function (error) {
+	// 	console.error('Lỗi khi lấy dữ liệu bài đăng sản phẩm:', error);
+	// });
 
 
-    //Hàm để cập nhật dữ liệu từ API và số trang khi chuyển đến trang mới
-		function loadViolationsData(page) {
-			$http.get(url + '/staff/postsproduct', { params: { page: page, size: $scope.pageSize } })
-				.then(function (response) {
+	//Hàm để cập nhật dữ liệu từ API và số trang khi chuyển đến trang mới
+	function loadViolationsData(page) {
+		$http.get(url + '/staff/postsproduct', { params: { page: page, size: $scope.pageSize } })
+			.then(function (response) {
+				$scope.listPosts = response.data;
+				updatePagination();
+			})
+			.catch(function (error) {
+				console.error('Lỗi khi lấy dữ liệu bài viết vi phạm:', error);
+			});
+	}
+
+	// Hàm để cập nhật số trang dựa vào số lượng bài viết vi phạm
+	function calculateTotalPages() {
+		$scope.totalPages = $scope.listPosts.totalPages;
+	}
+
+
+	// Hàm để chuyển đến trang trước
+	$scope.prevPage = function () {
+		if ($scope.currentPage > 0) {
+			$scope.currentPage--;
+			loadViolationsData($scope.currentPage);
+		}
+	};
+
+	// Hàm để chuyển đến trang kế tiếp
+	$scope.nextPage = function () {
+		if ($scope.currentPage < $scope.totalPages - 1) {
+			$scope.currentPage++;
+			loadViolationsData($scope.currentPage);
+		}
+	};
+
+	// Hàm để cập nhật số trang khi dữ liệu thay đổi
+	function updatePagination() {
+		calculateTotalPages();
+		// Đảm bảo rằng trang hiện tại không vượt quá tổng số trang
+		$scope.currentPage = Math.min($scope.currentPage, $scope.totalPages - 1);
+	}
+
+	// Gọi hàm để cập nhật dữ liệu từ API khi controller khởi tạo
+	loadViolationsData($scope.currentPage);
+
+	// Hàm để tạo mảng các trang cụ thể
+	$scope.getPagesArray = function () {
+		return Array.from({ length: $scope.totalPages }, (_, i) => i);
+	};
+
+	$scope.searchProduct = function () {
+		var name = $scope.searchText;
+		name = name.trim();
+		if (!name) {
+			// Nếu ô tìm kiếm rỗng, gán lại giá trị ban đầu cho biến $scope.listViolations.content
+			$scope.listPosts.content;
+			return;
+		}
+		$http.get(url + '/staff/searchProduct', { params: { name: name } })
+			.then(function (response) {
+				// Xử lý kết quả tìm kiếm ở đây
+				console.log(response.data);
+				$scope.listPosts.content = response.data;
+			})
+			.catch(function (error) {
+				console.error('Lỗi khi tìm kiếm bài viết:', error);
+			});
+	};
+
+	$scope.detailProduct = function (productId) {
+		// Gọi API để lấy thông tin chi tiết bài viết dựa vào postId
+		$http.get(url + '/staff/postsproduct/detailProduct/' + productId)
+			.then(function (response) {
+				$scope.product = response.data;
+				console.log($scope.product);
+				$('#exampleModal').modal('show'); // Hiển thị modal khi có dữ liệu bài viết
+			})
+			.catch(function (error) {
+				console.error('Lỗi khi lấy thông tin chi tiết bài viết:', error);
+			});
+	};
+
+	$scope.accept = function (productId) {
+		
+		Swal.fire({
+			text: 'Bạn có chắc muốn chấp nhận sản phẩm này không?',
+			icon: 'warning',
+			confirmButtonText: 'Có, chắc chắn',
+			showCancelButton: true,
+			confirmButtonColor: '#159b59',
+			cancelButtonColor: '#d33'
+		}).then((result) => {
+			if (result.isConfirmed) {				
+				$http({
+					method: 'POST',
+					url: url + '/staff/postsproduct/accept/' + productId
+				}).then(function (response) {
+					// Cập nhật dữ liệu mới nhận được từ server
 					$scope.listPosts = response.data;
-					updatePagination();
-				})
-				.catch(function (error) {
-					console.error('Lỗi khi lấy dữ liệu bài viết vi phạm:', error);
+
+					Swal.fire({
+						position: 'top',
+						icon: 'success',
+						text: 'Chấp nhận yêu cầu đăng sản phẩm thành công!',
+						showConfirmButton: false,
+						timer: 1800
+					});
+				}).catch(function (error) {
+					console.error('Lỗi khi chấp nhận yêu cầu các bài viết:', error);
+
+					Swal.fire({
+						position: 'top',
+						icon: 'error',
+						text: 'Không chấp nhận yêu cầu được!',
+						showConfirmButton: false,
+						timer: 1800
+					});
 				});
-		}
-
-		// Hàm để cập nhật số trang dựa vào số lượng bài viết vi phạm
-		function calculateTotalPages() {
-			$scope.totalPages = $scope.listPosts.totalPages;
-		}
-
-
-		// Hàm để chuyển đến trang trước
-		$scope.prevPage = function () {
-			if ($scope.currentPage > 0) {
-				$scope.currentPage--;
-				loadViolationsData($scope.currentPage);
 			}
-		};
-
-		// Hàm để chuyển đến trang kế tiếp
-		$scope.nextPage = function () {
-			if ($scope.currentPage < $scope.totalPages - 1) {
-				$scope.currentPage++;
-				loadViolationsData($scope.currentPage);
-			}
-		};
-
-		// Hàm để cập nhật số trang khi dữ liệu thay đổi
-		function updatePagination() {
-			calculateTotalPages();
-			// Đảm bảo rằng trang hiện tại không vượt quá tổng số trang
-			$scope.currentPage = Math.min($scope.currentPage, $scope.totalPages - 1);
-		}
-
-        // Gọi hàm để cập nhật dữ liệu từ API khi controller khởi tạo
-		loadViolationsData($scope.currentPage);
-
-        // Hàm để tạo mảng các trang cụ thể
-		$scope.getPagesArray = function () {
-			return Array.from({ length: $scope.totalPages }, (_, i) => i);
-		};
-
-        //Xử lý các checkbox
-		$scope.selectAll = function () {
-			var isChecked = $scope.selectAllCheckbox;
-			angular.forEach($scope.listPosts.content, function (violation) {
-				violation.checked = !isChecked;
-			});
-			$scope.updateSelectedCount();
-		};
-
-		$scope.updateSelectedCount = function () {
-			var selectedCount = $scope.listPosts.content.filter(function (violation) {
-				return violation.isVisible && violation.checked; // Chỉ đếm các checkbox được chọn trong các dòng không ẩn
-			}).length;
-			$scope.selectedCountText = selectedCount;
-		};
+		});
+	};
 
 
-		$scope.checkboxClicked = function () {
-			var allChecked = true;
-			angular.forEach($scope.listPosts.content, function (violation) {
-				if (!violation.checked) {
-					allChecked = false;
-				}
-			});
-			$scope.selectAllCheckbox = allChecked;
-			$scope.updateSelectedCount();
-		};
-
-        
+	$scope.reloadPage = function () {
+		location.reload();
+	}
 
 })
