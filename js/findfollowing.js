0f6d186fbbfc0f4a10b1623e424ff2335eda87c8
Huynh-Duy-Quy <huynhduyquy2001@gmail.com>
2023-12-01 18:06:39 +0700

    Update findfollowing.js

--- js/findfollowing.js
+++ js/findfollowing.js
@@ -1,6 +1,6 @@
 angular.module('myApp', [])
     .controller('myCtrl', function ($scope, $http) {
-        $http.get('/findfollowing')
+        $http.get('http://localhost:8080/findfollowing')
             .then(function (response) {
                 var Posts = response.data;
                 $scope.Posts = Posts;
